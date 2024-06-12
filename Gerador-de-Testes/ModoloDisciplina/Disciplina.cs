ab9aaac8292f90ba053aa3326bc710783f3e8c8f
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-06-12 10:18:30 -0300

    Classe, repositorio e tela implementados

--- Gerador-de-Testes/ModoloDisciplina/Disciplina.cs
+++ Gerador-de-Testes/ModoloDisciplina/Disciplina.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Gerador_de_Testes.ModoloMateria;
+using Gerador_de_Testes.WinApp.Compartilhado;
 
 namespace Gerador_de_Testes.ModoloDisciplina
 {
-    public class Disciplina
+    public class Disciplina : EntidadeBase
     {
+        //Variaveis da classe
+        public string Nome {  get; set; }
+        public List<Materia> Materias { get; set; }
+
+        //Construtor completo
+        public Disciplina(string nome)
+        {
+            Nome = nome;
+            Materias = new List<Materia>();
+        }
+        //Construtor vazil
+        public Disciplina() { }
+
+        public override List<string> Validar()
+        {
+            List<string> erros = new List<string>();
+
+            if (string.IsNullOrEmpty(this.Nome))
+                erros.Add("O campo 'Nome' é obrigatório");
+
+            if(this.Nome.Length < 3)
+                    erros.Add("O campo 'Nome' deve conter no mínimo 3 caracteres");
+
+            return erros;
+        }
+
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            Disciplina novo = (Disciplina)novoRegistro;
+
+            this.Nome = novo.Nome;
+            this.Materias = novo.Materias;
+        }
     }
 }
---
+++ Gerador-de-Testes/ModoloDisciplina/IRepositorioDisciplina.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerador_de_Testes.ModoloDisciplina
+{
+    public interface IRepositorioDisciplina
+    {
+        void Cadastrar(Disciplina novaDisciplina);
+        bool Editar(int id, Disciplina disciplinaEditado);
+        bool Excluir(int id);
+        Disciplina SelecionarPorId(int idSelecionado);
+        List<Disciplina> SelecionarTodos();
+    }
+}
