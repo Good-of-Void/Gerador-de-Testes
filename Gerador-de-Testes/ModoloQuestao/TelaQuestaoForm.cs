b909ee5f327e986cb896a821d854f511ed28a5f7
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-06-12 14:31:08 -0300

    modoloQuestao- tabela ok

---
+++ Gerador-de-Testes/ModoloQuestao/TelaQuestaoForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gerador_de_Testes.ModoloQuestao
+{
+    public partial class TelaQuestaoForm : Form
+    {
+        public TelaQuestaoForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
